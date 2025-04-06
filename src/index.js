27ec3bc751e59bf58f8b3b99ee26efb90177f30d
Mikroud0239 <bayrachuk@gmail.com>
2025-04-07 03:23:28 +0800

    fix: added missing index.js and index.html

---
+++ src/index.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="mn">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="theme-color" content="#000000" />
+    <title>Setgel Züi App</title>
+  </head>
+  <body>
+    <noscript>Javascript идэвхжүүлээгүй байна. Та идэвхжүүлнэ үү.</noscript>
+    <div id="root"></div>
+  </body>
+</html>
